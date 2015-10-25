4d55bfffba4929c3615eb959ebc76d959054bd05
Éric Massé <Ericmas001@gmail.com>
2015-10-25 15:30:25 -0400

    Using pokemons for real

--- Com.Ericmas001.WebService/Controllers/PokemonsController.cs
+++ Com.Ericmas001.WebService/Controllers/PokemonsController.cs
@@ -16,29 +16,13 @@ namespace Com.Ericmas001.WebService.Controllers
         // GET api/pokemons
         public IEnumerable<Pokemon> Get()
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = "Com.Ericmas001.WebService.Content.Pokemons.txt";
-
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                string result = reader.ReadToEnd();
-                Pokemon[] pokemons = result.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(x => x.Split(';')).GroupBy(x => x[0]).Select(GeneratePokemon).ToArray();
-                return pokemons;
-            }
-        }
-
-        private Pokemon GeneratePokemon(IGrouping<string, string[]> arg)
-        {
-            var master = arg.First();
-            var types = Join(", ", arg.Select(x => x[3]));
-            return new Pokemon() {Id = int.Parse(master[0]),Name=master[1],Photo=master[4],Type=types};
+            return Pokemon.AllPokemons;
         }
 
         // GET api/pokemons/5
         public Pokemon Get(int id)
         {
-            var p = Pokemon.PokemonListMock.SingleOrDefault(x => x.Id == id);
+            var p = Pokemon.AllPokemons.SingleOrDefault(x => x.Id == id);
             return p ?? new Pokemon
                 {
                     Id = id,
--- Com.Ericmas001.WebService/Models/Pokemon.cs
+++ Com.Ericmas001.WebService/Models/Pokemon.cs
@@ -1,82 +1,48 @@
-﻿namespace Com.Ericmas001.WebService.Models
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using static System.String;
+
+namespace Com.Ericmas001.WebService.Models
 {
     public class Pokemon
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Type { get; set; } 
+        public string Type { get; set; }
         public string Photo { get; set; }
 
-        public static Pokemon[] PokemonListMock = {
-                new Pokemon
+        private static Pokemon[] m_AllPokemons;
+
+        public static Pokemon[] AllPokemons => m_AllPokemons ?? GetPokemons();
+
+        private static Pokemon[] GetPokemons()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Com.Ericmas001.WebService.Content.Pokemons.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
                 {
-                    Id = 1,
-                    Name="Bulbasaur",
-                    Type = "Grass / Poison",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png"
-                },
-                new Pokemon
-                {
-                    Id = 4,
-                    Name="Charmander",
-                    Type = "Fire",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png"
-                },
-                new Pokemon
-                {
-                    Id = 7,
-                    Name="Squirtle",
-                    Type = "Water",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png"
-                },
-                new Pokemon
-                {
-                    Id = 10,
-                    Name="Caterpie",
-                    Type = "Bug",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/5/5d/010Caterpie.png/250px-010Caterpie.png"
-                },
-                new Pokemon
-                {
-                    Id = 25,
-                    Name="Pikachu",
-                    Type = "Electric",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png"
-                },
-                new Pokemon
-                {
-                    Id = 43,
-                    Name="Oddish",
-                    Type = "Grass / Poison",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/4/43/043Oddish.png/250px-043Oddish.png"
-                },
-                new Pokemon
-                {
-                    Id = 52,
-                    Name="Meowth",
-                    Type = "Normal",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/d/d6/052Meowth.png/250px-052Meowth.png"
-                },
-                new Pokemon
-                {
-                    Id = 54,
-                    Name="Psyduck",
-                    Type = "Water",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/5/53/054Psyduck.png/250px-054Psyduck.png"
-                },
-                new Pokemon
-                {
-                    Id = 129,
-                    Name="Magikarp",
-                    Type = "Water",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/0/02/129Magikarp.png/250px-129Magikarp.png"
-                },
-                new Pokemon
-                {
-                    Id = 143,
-                    Name="Snorlax",
-                    Type = "Normal",
-                    Photo = "http://cdn.bulbagarden.net/upload/thumb/f/fb/143Snorlax.png/250px-143Snorlax.png"
-                }};
+                    string result = reader.ReadToEnd();
+                    Pokemon[] pokemons = result.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(x => x.Split(';')).GroupBy(x => x[0]).Select(GeneratePokemon).ToArray();
+                    m_AllPokemons = pokemons;
+                    return pokemons;
+                }
+        }
+
+        private static Pokemon GeneratePokemon(IGrouping<string, string[]> arg)
+        {
+            var master = arg.First();
+            var types = Join(", ", arg.Select(x => x[3]));
+            return new Pokemon
+            {
+                Id = int.Parse(master[0]),
+                Name = master[1],
+                Photo = master[4],
+                Type = types
+            };
+        }
     }
 }
