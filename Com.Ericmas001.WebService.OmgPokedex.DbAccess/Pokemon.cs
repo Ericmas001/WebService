82a5c65045fca9624d8c94a1759c223ae167210e
Éric Massé <Ericmas001@gmail.com>
2015-11-07 08:01:49 -0500

    Adding first layer of entity framework for omg pokemon

---
+++ Com.Ericmas001.WebService.OmgPokedex.DbAccess/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Com.Ericmas001.WebService.OmgPokedex.DbAccess
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Photo { get; set; }
+
+        public void AddToDatabase()
+        {
+            
+        }
+    }
+}
---
+++ PokemonStorer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.Ericmas001.WebService.OmgPokedex.DbAccess;
+using static System.String;
+
+namespace PokemonStorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.ReadAllLines(@"D:\Pokemons.txt").Select(x => x.Split(';')).GroupBy(x => x[0]).Select(GeneratePokemon).Take(10).ToList().ForEach(x => x.AddToDatabase());
+        }
+
+        private static Pokemon GeneratePokemon(IGrouping<string, string[]> arg)
+        {
+            var master = arg.First();
+            var types = Join(" / ", arg.Select(x => x[3]));
+            return new Pokemon
+            {
+                Id = int.Parse(master[0]),
+                Name = master[1],
+                Photo = master[4],
+                Type = types
+            };
+        }
+    }
+}
